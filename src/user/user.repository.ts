767ad2ba2173f6d22fd40f81d143ccea0fddb9e1
Kimcheolhui <cjfgml0306@naver.com>
2024-11-20 11:15:58 +0900

    chore: add function return type

--- src/user/user.repository.ts
+++ src/user/user.repository.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { User } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
 export class UserRepository {
   constructor(private readonly prismaService: PrismaService) {}
 
-  async findUserOrCreate({ uuid, name }) {
+  async findUserOrCreate({ uuid, name }): Promise<User> {
     const user = await this.prismaService.user.findUnique({
       where: { uuid },
     });
