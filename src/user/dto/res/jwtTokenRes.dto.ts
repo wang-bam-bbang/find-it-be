3750eca21b26ef181c08fac1f105e71c66796ff4
Kimcheolhui <cjfgml0306@naver.com>
2024-11-23 01:03:35 +0900

    fix: modify decorator from IsEmpty to IsNotEmpty

--- src/user/dto/res/jwtTokenRes.dto.ts
+++ src/user/dto/res/jwtTokenRes.dto.ts
@@ -1,7 +1,7 @@
-import { IsEmpty, IsString } from 'class-validator';
+import { IsNotEmpty, IsString } from 'class-validator';
 
 export class JwtTokenResDto {
   @IsString()
-  @IsEmpty()
+  @IsNotEmpty()
   access_token: string;
 }
