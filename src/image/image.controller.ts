5b2b775da5c464c5ee15c865f3b2ad235a55e3c3
Kimcheolhui <cjfgml0306@naver.com>
2024-11-30 01:37:51 +0900

    docs: add image module docs

--- src/image/image.controller.ts
+++ src/image/image.controller.ts
@@ -2,19 +2,63 @@ import {
   Controller,
   Post,
   UploadedFiles,
+  UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
 import { FilesInterceptor } from '@nestjs/platform-express';
 
 import { ImageService } from './image.service';
+import {
+  ApiBearerAuth,
+  ApiBody,
+  ApiConsumes,
+  ApiInternalServerErrorResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
+import { IdPGuard } from 'src/user/guard/idp.guard';
+import { GetUser } from 'src/user/decorator/get-user.decorator';
+import { User } from '@prisma/client';
 
 @Controller('image')
 export class ImageController {
   constructor(private imageService: ImageService) {}
 
+  @ApiOperation({
+    summary: 'upload images',
+    description: 'upload images',
+  })
+  @ApiConsumes('multipart/form-data')
+  @ApiBody({
+    description: 'List of images to upload',
+    schema: {
+      type: 'object',
+      properties: {
+        images: {
+          type: 'array',
+          items: {
+            type: 'string',
+            format: 'binary',
+          },
+        },
+      },
+    },
+  })
+  @ApiOkResponse({
+    type: [String],
+    description: 'Return keys of uploaed images',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+  })
+  @ApiBearerAuth('access-token')
   @Post('upload')
+  @UseGuards(IdPGuard)
   @UseInterceptors(FilesInterceptor('images'))
   async uploadImages(
+    @GetUser() user: User,
     @UploadedFiles() files: Express.Multer.File[],
   ): Promise<string[]> {
     return this.imageService.uploadImages(files);
--- src/image/image.module.ts
+++ src/image/image.module.ts
@@ -3,9 +3,10 @@ import { ImageController } from './image.controller';
 import { ImageService } from './image.service';
 import { ConfigModule } from '@nestjs/config';
 import { S3Client } from '@aws-sdk/client-s3';
+import { PassportModule } from '@nestjs/passport';
 
 @Module({
-  imports: [ConfigModule, S3Client],
+  imports: [PassportModule, ConfigModule, S3Client],
   providers: [ImageService, S3Client],
   controllers: [ImageController],
   exports: [ImageService],
