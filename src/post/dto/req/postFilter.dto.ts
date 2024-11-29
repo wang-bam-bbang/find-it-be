e8bdfb2696a8ca3b7f8eabb5151a63cac7f3ae75
Kimcheolhui <cjfgml0306@naver.com>
2024-11-29 16:27:42 +0900

    docs: add post module docs

--- src/post/dto/req/postFilter.dto.ts
+++ src/post/dto/req/postFilter.dto.ts
@@ -1,25 +1,55 @@
+import { ApiProperty } from '@nestjs/swagger';
 import { ItemCategory, PostStatus, PostType } from '@prisma/client';
-import { Transform } from 'class-transformer';
+import { Transform, Type } from 'class-transformer';
 import { IsEnum, IsInt, IsOptional, Max } from 'class-validator';
 
 export class PostFilterDto {
+  @ApiProperty({
+    enum: ItemCategory,
+    enumName: 'ItemCategory',
+    description: 'Item category',
+    example: ItemCategory.ELECTRONICS,
+  })
   @IsEnum(ItemCategory)
   @IsOptional()
   category?: ItemCategory;
 
+  @ApiProperty({
+    enum: PostType,
+    enumName: 'PostType',
+    description: 'Type of post (FOUND or LOST)',
+    example: PostType.FOUND,
+  })
   @IsEnum(PostType)
   @IsOptional()
   type?: PostType;
 
+  @ApiProperty({
+    enum: PostStatus,
+    enumName: 'PostStatus',
+    description: 'Post status',
+    example: PostStatus.IN_PROGRESS,
+  })
   @IsEnum(PostStatus)
   @IsOptional()
   status?: PostStatus;
 
+  @ApiProperty({
+    type: Number,
+    description: 'Pagination cursor for next set of results',
+    example: 17,
+  })
   @Transform(({ value }) => parseInt(value, 10), { toClassOnly: true })
   @IsInt()
   @IsOptional()
   cursor?: number;
 
+  @ApiProperty({
+    type: Number,
+    description: 'Number of posts to return (maximum 20)',
+    example: 10,
+  })
+  @Type(() => Number)
   @IsInt()
   @IsOptional()
   @Max(20)
--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -18,11 +18,32 @@ import { PostService } from './post.service';
 import { PostListDto, PostResponseDto } from './dto/res/postRes.dto';
 import { UpdatePostDto } from './dto/req/updatePost.dto';
 import { PostFilterDto } from './dto/req/postFilter.dto';
+import {
+  ApiBearerAuth,
+  ApiInternalServerErrorResponse,
+  ApiNoContentResponse,
+  ApiOkResponse,
+  ApiOperation,
+  ApiTags,
+  ApiUnauthorizedResponse,
+} from '@nestjs/swagger';
 
+@ApiTags('post')
 @Controller('post')
 export class PostController {
   constructor(private postService: PostService) {}
 
+  @ApiOperation({
+    summary: 'get post list',
+    description: 'get post list with filter query',
+  })
+  @ApiOkResponse({
+    type: PostListDto,
+    description: 'Return Post List',
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+  })
   @Get('list')
   async getPostList(
     @Query() postFilterDto: PostFilterDto,
@@ -30,12 +51,38 @@ export class PostController {
     return this.postService.getPostList(postFilterDto);
   }
 
+  @ApiOperation({
+    summary: 'get my post list',
+    description: 'get all my post list with',
+  })
+  @ApiOkResponse({
+    type: PostListDto,
+    description: 'Return My Post List',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+  })
+  @ApiBearerAuth('access-token')
   @Get('my-posts')
   @UseGuards(IdPGuard)
   async getMyPosts(@GetUser() user: User): Promise<PostListDto> {
     return this.postService.getMyPostList(user.uuid);
   }
 
+  @ApiOperation({
+    summary: 'create post',
+    description: 'create post with token',
+  })
+  @ApiOkResponse({
+    type: PostListDto,
+    description: 'Return Created Post',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+  })
+  @ApiBearerAuth('access-token')
   @Post()
   @UseGuards(IdPGuard)
   async createPost(
@@ -45,6 +92,18 @@ export class PostController {
     return this.postService.createPost(createPostDto, user.uuid);
   }
 
+  @ApiOperation({
+    summary: 'get post',
+    description: 'get post by id',
+  })
+  @ApiOkResponse({
+    type: PostResponseDto,
+    description: 'Return Single Post matched with id',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+  })
   @Get(':id')
   async getPostByid(
     @Param('id', ParseIntPipe) id: number,
@@ -52,16 +111,39 @@ export class PostController {
     return this.postService.getPostById(id);
   }
 
+  @ApiOperation({
+    summary: 'update post',
+    description: 'update my post',
+  })
+  @ApiOkResponse({
+    type: PostResponseDto,
+    description: 'Return Single Post matched with id',
+  })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+  })
+  @ApiBearerAuth('access-token')
   @Patch(':id')
   @UseGuards(IdPGuard)
   async updatePost(
     @Param('id', ParseIntPipe) id: number,
     @GetUser() user: User,
     @Body() updatePostDto: UpdatePostDto,
-  ) {
+  ): Promise<PostResponseDto> {
     return this.postService.updatePost(id, updatePostDto, user.uuid);
   }
 
+  @ApiOperation({
+    summary: 'delete post',
+    description: 'delete my post',
+  })
+  @ApiNoContentResponse({ description: 'No content returned' })
+  @ApiUnauthorizedResponse({ description: 'Unauthorized' })
+  @ApiInternalServerErrorResponse({
+    description: 'Internal Server Error',
+  })
+  @ApiBearerAuth('access-token')
   @Delete(':id')
   @UseGuards(IdPGuard)
   async deletePost(
