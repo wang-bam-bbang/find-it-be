8b06d343916b5639f34b9ffaacbef962f0c9975c
Kimcheolhui <cjfgml0306@naver.com>
2024-11-29 15:22:26 +0900

    docs: add swagger docs module

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
@@ -11,6 +13,32 @@ async function bootstrap() {
     }),
   );
 
+  const config = new DocumentBuilder()
+    .setTitle('Fint-it API')
+    .setDescription(
+      'Backend API for Find-it\n[GitHub](https://github.com/wang-bam-bbang/find-it-be)',
+    )
+    .setVersion('1.0')
+    .addTag('Fint-it')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        in: 'header',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api/docs', app, document, {
+    swaggerOptions: {
+      displayRequestDuration: true,
+    },
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
+
 bootstrap();
